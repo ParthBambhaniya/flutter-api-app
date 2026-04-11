4aaa9e0ea15111a44e9b3acef24cda6496620030
Parth <parthjbambhnaiya2003@gmail.com>
2026-04-11 18:32:31 +0530

    I am learn for flutter rest API part 1

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rest_api_call/screen/home.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? Key}) : super(key: Key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: HomeScree(),
+    );
+  }
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScree extends StatefulWidget {
+  const HomeScree({super.key});
+
+  @override
+  State<HomeScree> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomeScree> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+    return Scaffold();
+  }
+}
