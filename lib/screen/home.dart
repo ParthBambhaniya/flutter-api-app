6f22a4c21ca849936e23efdf6973b617fd6127ab
Parth <parthjbambhnaiya2003@gmail.com>
2026-04-24 20:30:22 +0530

    Flutter API Learning 3 part created

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,8 +1,9 @@
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_rest_api_call/model/user.dart' show User, UserName;
-import 'package:http/http.dart' as http;
+import 'package:flutter_rest_api_call/services/user_api.dart';
+import 'package:http/http.dart';
+
 
 class HomeScree extends StatefulWidget {
   const HomeScree({super.key});
@@ -13,6 +14,12 @@ class HomeScree extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScree> {
   List<User> users = [];
+
+  @override
+  void initState(){
+     super.initState();
+     fetchUsers(); 
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -26,45 +33,52 @@ class _HomeScreenState extends State<HomeScree> {
           final user = users[index];
           //final color = user.gender == 'male' ? Colors.amber :Colors.blueGrey;
           return ListTile(
-              title: Text(user.name.first),
+              title: Text(user.fullName),
               subtitle: Text(user.phone),
               //tileColor: color,
           );
         }
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: fetchUsers,
-        ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: fetchUsers,
+      //   ),
     );
   }
 
-  void fetchUsers() async{
-    print('FetchUsers Called');
-    const url= 'https://randomuser.me/api/?results=100';
-    final uri = Uri.parse(url);
-    final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
-    final results = json['results'] as List<dynamic>;
-    final transformed = results.map((e){
-      final name = UserName(
-        title: e['name'] ['title'], 
-        first: e['name'] ['first'], 
-        last: e['name'] ['last'],
-        );
-        return User(
-          cell: e['cell'],
-          email: e['email'],
-          gender: e['gender'],
-          nat: e['nat'],
-          phone: e['phone'],
-          name: name,
-        );
-      }).toList();
-    setState(() {
-      //users = json['results'];
-      users = transformed;
-    });
-    print('FetchUsers Completed');
+  Future<void> fetchUsers() async{
+   final response = await UserApi.fetchUsers();
+   setState(() {
+     users = response;
+   });
   }
+
+  // Future<void> fetchUsers() async{
+  //   print('FetchUsers Called');
+  //   const url= 'https://randomuser.me/api/?results=100';
+  //   final uri = Uri.parse(url);
+  //   final response = await http.get(uri);
+  //   final body = response.body;
+  //   final json = jsonDecode(body);
+  //   final results = json['results'] as List<dynamic>;
+  //   final transformed = results.map((e){
+  //     final name = UserName(
+  //       title: e['name'] ['title'], 
+  //       first: e['name'] ['first'], 
+  //       last: e['name'] ['last'],
+  //       );
+  //       return User(
+  //         cell: e['cell'],
+  //         email: e['email'],
+  //         gender: e['gender'],
+  //         nat: e['nat'],
+  //         phone: e['phone'],
+  //         name: name,
+  //       );
+  //     }).toList();
+  //   setState(() {
+  //     //users = json['results'];
+  //     users = transformed;
+  //   });
+  //   print('FetchUsers Completed');
+  // }
 }
---
+++ lib/services/user_api.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:flutter_rest_api_call/model/user.dart';
+import 'package:flutter_rest_api_call/model/user_name.dart';
+import 'package:http/http.dart' as http;
+
+class UserApi {
+    static Future<List<User>> fetchUsers() async{
+    const url= 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    final results = json['results'] as List<dynamic>;
+    final users = results.map((e){
+      final name = UserName(
+        title: e['name'] ['title'], 
+        first: e['name'] ['first'], 
+        last: e['name'] ['last'],
+        );
+        return User(
+          cell: e['cell'],
+          email: e['email'],
+          gender: e['gender'],
+          nat: e['nat'],
+          phone: e['phone'],
+          name: name,
+        );
+      }).toList();
+    return users;
+  }
+}
